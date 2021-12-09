dd455f94534d1100d84d8992825bfb7009918560
Gregor Lichtner <gregor.lichtner@charite.de>
2021-12-09 23:53:14 +0100

    updated fhir downloader

--- download-fhir-r5-ci.sh
+++ download-fhir-r5-ci.sh
@@ -1,29 +1,28 @@
-#!/bin/sh
-fhir_path="$HOME/.fhir/packages/hl7.fhir.r5.core#4.6.0"
-fhir_path_current="$HOME/.fhir/packages/hl7.fhir.r5.core#current"
-fhir_expansion_path="$HOME/.fhir/packages/hl7.fhir.r5.expansions#4.6.0"
-fhir_examples_path="$HOME/.fhir/packages/hl7.fhir.r5.examples#4.6.0"
+#!/bin/bash
 
-fhir_package_name="hl7.fhir.r5.core.tgz"
-fhir_expansion_name="hl7.fhir.r5.expansions.tgz"
-fhir_examples_name="hl7.fhir.r5.examples.tgz"
+declare -a packages=(
+  "hl7.fhir.r5.core.tgz"
+  "hl7.fhir.r5.expansions.tgz"
+#  "hl7.fhir.r5.examples.tgz"
+)
 
-mkdir -p $fhir_path
-cd $fhir_path
-wget https://build.fhir.org/$fhir_package_name -O $fhir_package_name
-tar -zxvf $fhir_package_name
-rm $fhir_package_name
+fhir_version="4.6.0"
 
-ln -s $fhir_path $fhir_path_current
+echo $HOME
+mkdir -p $HOME/.fhir
+mkdir -p $HOME/.fhir/packages
 
-mkdir -p $fhir_expansion_path
-cd $fhir_expansion_path
-wget https://build.fhir.org/$fhir_expansion_name -O $fhir_expansion_name
-tar -zxvf $fhir_expansion_name
-rm $fhir_expansion_name
+for fhir_package_name in "${packages[@]}"
+do
+  basename="$(basename $fhir_package_name .tgz)"
+  fhir_path="$HOME/.fhir/packages/$basename#$fhir_version"
+  fhir_path_current="$HOME/.fhir/packages/$basename#current"
+  echo "$fhir_path"
+  mkdir -p $fhir_path
+  cd $fhir_path
+  wget -q https://build.fhir.org/$fhir_package_name -O $fhir_package_name
+  tar -zxf $fhir_package_name
+  rm $fhir_package_name
+done
 
-#mkdir -p $fhir_examples_path
-#cd $fhir_examples_path
-#wget https://build.fhir.org/$fhir_examples_name -O $fhir_examples_name
-#tar -zxvf $fhir_examples_name
-#rm $fhir_examples_name
+ls -al $HOME/.fhir/packages
