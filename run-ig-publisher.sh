fbedc898e7c98839e180e0ec2323588465b3779d
Gregor Lichtner <gregor.lichtner@charite.de>
2022-09-15 12:07:59 +0200

    update to 5.0.0-ballot

--- run-ig-publisher.sh
+++ run-ig-publisher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # workaround for Sushi 2.6.1 not using the new ImplementationGuide structure (which changed definition.page.nameUrl to definition.page.name)
-./download-fhir-r5-ci.sh
 sushi .
 sed -i 's/"nameUrl"/"name"/g' fsh-generated/resources/ImplementationGuide-cpg.ceosys.de.json
+sed -i 's/"exampleBoolean"/"isExample"/g' fsh-generated/resources/ImplementationGuide-cpg.ceosys.de.json
+sed -i 's/"code": "\([^"]\+\)"/"code": { "code": "\1"}/g' fsh-generated/resources/ImplementationGuide-cpg.ceosys.de.json
+sed -i 's/"exampleCanonical": "\([^"]\+\)"/"isExample": "true",\n\t\t\t\t"profile": ["\1"]/g' fsh-generated/resources/ImplementationGuide-cpg.ceosys.de.json
 curl -L https://github.com/CEOsys/fhir-ig-publisher/releases/latest/download/publisher.jar -o ./input-cache/publisher.jar --create-dirs
 java -jar ./input-cache/publisher.jar publisher -ig . -no-sushi
