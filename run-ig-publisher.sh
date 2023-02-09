10fe9bf3ac2a53eb41e4d3d38659d7eef51ed4ce
Gregor Lichtner <gregor.lichtner@charite.de>
2023-02-09 11:44:56 +0100

    releaseLabel change

--- run-ig-publisher.sh
+++ run-ig-publisher.sh
@@ -18,8 +18,17 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
 targetUrl=$1
 version=$2
 
-# run sushi
-sushi .
+function set_release_label() {
+  local release_label=$1
+  local sushi_config_file="sushi-config.yaml"
+
+  if [ ! -f "$sushi_config_file" ]; then
+    echo "Error: File $sushi_config_file does not exist."
+    return 1
+  fi
+
+  sed -i "s/^releaseLabel: .*/releaseLabel: $release_label/g" "$sushi_config_file"
+}
 
 # download publisher
 curl -L https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar -o ./input-cache/publisher.jar --create-dirs
@@ -31,12 +40,15 @@ if ! test -f "$publisher"; then
 	exit 1
 fi
 
-
 if [[ "$version" == *"-snapshot"* ]]; then
   # snapshot build
+  echo "Running snapshot build..."
+  set_release_label "ci-build" #  the continuous integration build release (not stable); see #https://fshschool.org/docs/sushi/configuration/#fsh-and-ig-processing-minimum-configuration
   java -jar $publisher -ig . -no-sushi $*
 else
   # release build
+  echo "Running release build..."
+  set_release_label "qa-preview" # frozen snapshot for non-ballot feedback; see #https://fshschool.org/docs/sushi/configuration/#fsh-and-ig-processing-minimum-configuration
   java -jar $publisher -ig . -no-sushi -publish $targetUrl $*
   find ./output -name "*.html" -exec sed -i 's/Publication Build: This will be filled in by the publication tooling/This page is part of the CPG-on-EBMonFHIR Implementation Guide ($2)./g' {} +
 fi
